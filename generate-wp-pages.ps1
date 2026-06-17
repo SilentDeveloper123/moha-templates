54d601c8d6a0bb06cad1c391a2f17debe0ba30ab
Himal Sarathchandra <himalhp100@gmail.com>
2026-06-18 02:43:52 +0530

    2026-06-18 Theme Fixes - Himal

--- generate-wp-pages.ps1
+++ generate-wp-pages.ps1
@@ -12,10 +12,10 @@ $head = @'
 <script src="https://cdn.tailwindcss.com"></script>
 <script>tailwind.config={theme:{extend:{colors:{navy:'#0A1931',green:'#43c000',steel:'#2D5F8A',maroon:'#801200'}}}}</script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glightbox@3.3.0/dist/css/glightbox.min.css">
 <link rel="stylesheet" href="css/style.css">
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-<script src="https://cdn.jsdelivr.net/gh/mcstudios/glightbox/dist/js/glightbox.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/glightbox@3.3.0/dist/js/glightbox.min.js"></script>
 <script src="js/main.js" defer></script>
 </head>
 <body class="bg-gray-50 text-gray-800 antialiased">
